b51fc81528b0800415bb7c96c811e482ba8890de
Alex <alexnjoroge102@gmail.com>
2025-03-17 15:02:37 +0300

    build changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom"], // Add this line
+    },
+  },
 })
